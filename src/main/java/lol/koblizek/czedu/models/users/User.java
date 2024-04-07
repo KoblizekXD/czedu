0c35881b12e4ed089de8cf9cefbd99fcf046c677
KoblizekXD <janprokupek@centrum.cz>
2024-04-07 15:02:51 +0200

    Proper annotation for remaining columns

--- src/main/java/lol/koblizek/czedu/models/users/User.java
+++ src/main/java/lol/koblizek/czedu/models/users/User.java
@@ -3,13 +3,13 @@ package lol.koblizek.czedu.models.users;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import lol.koblizek.czedu.util.validation.Nationality;
-import org.hibernate.annotations.Nationalized;
+import lol.koblizek.czedu.util.validation.PhoneNumber;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
-import java.util.Locale;
 
 @Inheritance(strategy = InheritanceType.JOINED)
 @Entity
@@ -19,19 +19,38 @@ public abstract class User implements UserDetails {
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "id", nullable = false)
     private Long id;
+    @Column(name = "first_name", nullable = false)
     private String firstName;
+    @Column(name = "last_name", nullable = false)
     private String lastName;
     @Email
+    @Column(nullable = false, unique = true)
     private String email;
+    @Column(nullable = false)
     private String password;
+    @PhoneNumber
     private String phoneNumber;
     @Nationality
+    @Column(nullable = false)
     private String nationality;
     @ElementCollection(targetClass = Role.class)
     private List<Role> roles;
+    private String gender;
+
+    // TODO: Add remaining columns when they're added, such as Classes, Schools etc.
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
     }
 }
--- src/main/java/lol/koblizek/czedu/util/validation/Nationality.java
+++ src/main/java/lol/koblizek/czedu/util/validation/Nationality.java
@@ -1,6 +1,7 @@
 package lol.koblizek.czedu.util.validation;
 
 import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -11,4 +12,7 @@ import java.lang.annotation.Target;
 @Target({ElementType.METHOD, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Nationality {
+    String message() default "Invalid phone number";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
 }
